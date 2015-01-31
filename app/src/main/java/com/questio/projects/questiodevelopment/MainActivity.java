75bad7a7e2b51f626a4ed0b004c2aca5ce487ee8
NingSama1vs100 <55130500008@st.sit.kmutt.ac.th>
2015-01-31 15:56:01 +0700

    V1!

--- app/src/main/java/com/questio/projects/questiodevelopment/MainActivity.java
+++ app/src/main/java/com/questio/projects/questiodevelopment/MainActivity.java
@@ -36,4 +36,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/questio/projects/questiodevelopment/QuestMap.java
@@ -0,0 +1,14 @@
+package com.questio.projects.questiodevelopment;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by ning jittima on 31/1/2558.
+ */
+public class QuestMap extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
