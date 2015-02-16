5907b0f6d00f01e77abcf657b488ab79e3afa95a
Chakrit <mynamecoad@gmail.com>
2015-02-16 11:47:44 +0700

    do in lab = =

---
+++ app/src/main/java/com/questio/projects/questiodevelopment/SectionSearch.java
@@ -0,0 +1,21 @@
+package com.questio.projects.questiodevelopment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CHAKRIT on 16/2/2558.
+ */
+public class SectionSearch extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_section_search, container, false);
+        Bundle args = getArguments();
+
+        return rootView;
+    }
+}
