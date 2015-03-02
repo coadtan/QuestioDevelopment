dcd66f18129c6dbffcdb691516be23a887817fc0
Chakrit <mynamecoad@gmail.com>
2015-03-02 14:07:54 +0700

    do in lab = =

--- app/src/main/java/com/questio/projects/questiodevelopment/SectionQuestmap.java
+++ app/src/main/java/com/questio/projects/questiodevelopment/SectionQuestmap.java
@@ -2,20 +2,42 @@ package com.questio.projects.questiodevelopment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v4.app.FragmentManager;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by CHAKRIT on 16/2/2558.
  */
 public  class SectionQuestmap extends Fragment {
+    private SupportMapFragment map;
+    private GoogleMap mMapView;
+    Fragment fragment;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_section_questmap, container, false);
-        Bundle args = getArguments();
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentManager fm = getChildFragmentManager();
+         fragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        if (fragment == null) {
+            fragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, fragment).commit();
+        }
 
-        return rootView;
+
+/***at this time google play services are not initialize so get map and add what ever you want to it in onResume() or onStart() **/
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (map == null) {
+            map = fragment.getMap();
+            map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+        }
+    }
+
+
 }
